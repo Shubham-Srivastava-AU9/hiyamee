3ddb093fdfcae6202ea138c5394c75f75c927290
Shubham Srivastava <cashivamsms@gmail.com>
2021-05-29 01:30:24 +0530

    ready for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 
 const app = express();
@@ -20,6 +21,14 @@ app.use('/api/jobs', require('./routes/jobsRoutes'));
 
 
 // Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+  }
 
 
 const PORT = process.env.PORT || 9000;
